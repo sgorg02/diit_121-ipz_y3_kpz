bbd601c647971ff7907cdb94b8b2bf4e88baa78f
sgorg02 <61521854+sgorg02@users.noreply.github.com>
2022-02-02 21:04:42 +0200

    .

---
+++ 3/data.pl
@@ -0,0 +1,4 @@
+:- dynamic family/4.
+
+family(mother, father, sister, brother).
+
--- 3/main.pl
+++ 3/main.pl
@@ -1,37 +1,52 @@
-eview:-student(FIO, B, A, G),
-writef('%32L|%11L|%11L|%3R\n', [FIO,B,A,G]), fail, true.
+:- dynamic family/4.
+%:- multifile stud/4.
 
-delete:-write("Enter group: "),
-read(G), 
-write("Enter FIO: "),
-read(N),
-retract(student(N, _, _, G)).
+family(mother, father, sister, brother).
 
-readfile:-consult('data.txt').
+add:-
+  write("Enter name mother: "), read(FIO),
+  write("Enter name father: "), read(B),
+  write("Enter name sister: "), read(A),
+  write("Enter name brother: "), read(G),
+  assert(family(FIO, B, A, G)).
 
-writefile:-tell('data.txt'),listing(student), told.
 
-findbygroup:-write("Enter group to find stdudents: "),
-read(G), writeln(G),
-student(FIO,_,_, G),
-writeln(FIO-G), fail; true.
+review:-
+  family(M, F, S, B),
+  writef('%11L|%11L|%11L|%3R\n', [M, F, S, B]), fail, true.
 
-menu:- repeat, 
-writef('
-  1-read db from file,\n
-  2-save db to file,\n
-  3-review db,\n
-  4-add new student to db,\n
-  5-delete student from db by name and group,\n
-  6-check students by class\n
-  '), read(Ch), 
-  menuitem(Ch), Ch = 0.
+delete:-
+  write("Enter name mother: "), read(N), 
+  write("Enter name father: "), read(G),
+  retract(family(N, G, _, _)).
+
+readfile:-
+  consult('data.pl').
+
+writefile:-
+  tell('data.pl'), listing(family), told.
+
+%findbygroup:-
+  %write("Enter group to find stdudents: "), read(G), 
+  %writeln(G), student(FIO,_,_, G),
+  %writeln(FIO-G), fail; true.
+
+menu:- 
+  repeat, 
+  writef('
+    1-read db from file,\n
+    2-save db to file,\n
+    3-review db,\n
+    4-add new family to db,\n
+    5-delete family from db by mother and father,\n
+    '), read(Ch), 
+    menuitem(Ch), Ch = 0.
+
 menuitem(1):-readfile.
 menuitem(2):-writefile.
 menuitem(3):-review.
 menuitem(4):-add.
 menuitem(5):-delete.
-menuitem(6):-findbygroup.
 menuitem(0).
 
 :-menu.
